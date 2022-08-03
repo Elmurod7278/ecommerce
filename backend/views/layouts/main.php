a2bd0b15e0812ecd0cc60cfdd198ca8831b69e5c
elmurodsuyunov01 <elmurodsuyunov02@gmail.com>
2022-08-03 20:14:48 +0500

    generate products crud

--- backend/views/layouts/main.php
+++ backend/views/layouts/main.php
@@ -55,7 +55,7 @@ AppAsset::register($this);
 
             <!-- Nav Item - Charts -->
             <li class="nav-item">
-                <a class="nav-link" href="charts.html">
+                <a class="nav-link" href="<?=\yii\helpers\Url::to(['product/index'])?>">
                     <i class="fas fa-fw fa-chart-area"></i>
                     <span>Products</span></a>
             </li>
@@ -300,7 +300,10 @@ AppAsset::register($this);
 
                 </nav>
                 <!-- End of Topbar -->
-                <?php echo $content ?>
+                <div class="p-3">
+                    <?php echo $content ?>
+                </div>
+
             </div>
             <!-- End of Main Content -->
 
