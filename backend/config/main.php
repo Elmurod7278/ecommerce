076e7d2ad7b9f0d0c71f0f308c49b1e8415b1c91
elmurodsuyunov01 <elmurodsuyunov02@gmail.com>
2022-08-03 18:59:21 +0500

    generate models and activequery

--- backend/config/main.php
+++ backend/config/main.php
@@ -11,7 +11,12 @@ return [
     'basePath' => dirname(__DIR__),
     'controllerNamespace' => 'backend\controllers',
     'bootstrap' => ['log'],
-    'modules' => [],
+    'modules' => [
+        'gii'=>[
+            'class'=>'yii\gii\Module',
+            'allowedIPs'=>['*']
+        ]
+    ],
     'components' => [
         'request' => [
             'csrfParam' => '_csrf-backend',
